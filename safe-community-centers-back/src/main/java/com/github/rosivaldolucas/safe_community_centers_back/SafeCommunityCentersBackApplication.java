f5b53853d76cb8914e3f9c76817c5386b132cbea
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-09-05 22:15:55 -0300

    back/config: iniciando o desafio Phoebus

---
+++ safe-community-centers-back/src/main/java/com/github/rosivaldolucas/safe_community_centers_back/SafeCommunityCentersBackApplication.java
@@ -0,0 +1,13 @@
+package com.github.rosivaldolucas.safe_community_centers_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCommunityCentersBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCommunityCentersBackApplication.class, args);
+	}
+
+}
---
+++ safe-community-centers-back/src/test/java/com/github/rosivaldolucas/safe_community_centers_back/SafeCommunityCentersBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rosivaldolucas.safe_community_centers_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeCommunityCentersBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
